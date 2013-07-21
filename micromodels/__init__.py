de2448d073da5583cd26ee99458fd11c4bcf80ab
Jason Kraus <zbyte64@gmail.com>
2013-07-20 22:14:38 -0700

    fix import

--- micromodels/__init__.py
+++ micromodels/__init__.py
@@ -2,6 +2,6 @@ from micromodels.models import Model
 from micromodels.fields import BaseField, CharField, IntegerField, FloatField,\
                     BooleanField, DateTimeField, DateField, TimeField,\
                     ModelField, ModelCollectionField, FieldCollectionField,\
-                    UUIDField, DecimalField, ValidationError
+                    UUIDField, DecimalField, FileField, ValidationError
 
 __version__ = '0.6.0'
