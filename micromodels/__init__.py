75affbd366dc17ee54fa03e8f39bbb348335e2d9
Jason Kraus <zbyte64@gmail.com>
2013-09-27 16:33:19 -0700

    version bump

--- micromodels/__init__.py
+++ micromodels/__init__.py
@@ -4,4 +4,4 @@ from micromodels.fields import BaseField, CharField, IntegerField, FloatField,\
                     ModelField, ModelCollectionField, FieldCollectionField,\
                     UUIDField, DecimalField, FileField, ValidationError
 
-__version__ = '0.6.0'
+__version__ = '0.6.1'
