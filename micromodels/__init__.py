51ab4f9758f0040f2e311559d35800f252fc4592
Antonio Dourado <tonnydourado@gmail.com>
2014-11-10 03:58:41 -0200

    Adding JSONField and removing FileField from public API, because I have no idea how it's supposed to work.

--- micromodels/__init__.py
+++ micromodels/__init__.py
@@ -2,6 +2,6 @@ from micromodels.models import Model
 from micromodels.fields import BaseField, CharField, IntegerField, FloatField,\
                     BooleanField, DateTimeField, DateField, TimeField,\
                     ModelField, ModelCollectionField, FieldCollectionField,\
-                    UUIDField, DecimalField, FileField, ValidationError
+                    UUIDField, DecimalField, JSONField, ValidationError
 
 __version__ = '0.6.2'
